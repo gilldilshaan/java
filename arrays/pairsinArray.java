29073e1dd8bd59732769fe9a24dbe3ab551061a1
gilldilshaan <gilldilshaan7@gmail.com>
2026-08-26 13:54:32 +0530

    java ques

--- arrays/pairsinArray.java
+++ arrays/pairsinArray.java
@@ -4,13 +4,16 @@ public class pairsinArray {
 
     public class ArrayCC{
         public static void printPairs(int numbers[]){
+            int tp=0;
             for(int i=0;i<numbers.length;i++){
                 int curr=numbers[i];
                 for(int j=i+1;j<numbers.length;j++){
                     System.out.print("("+curr+","+numbers[j]+")");
+                    tp++;
                 }
                 System.out.println();
             }
+            System.out.print(tp);
         }
     }
     public static void main(String[]args){
